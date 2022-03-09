08f342b2303534d4f5e41d9317a1e322eaf572f2
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-03-09 11:21:50 +0900

    Check task status

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from urllib import request
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 
 from tasks import factorial
 
@@ -10,8 +10,15 @@ def index_page():
     return render_template('index.html')
 
 
-@app.route('/create_task_factorial')
+@app.route('/factorial/')
 def create_task_factorial():
     n = int(request.args.get('n', 0))
     task = factorial.delay(n)
-    return redirect('/')
+    return redirect(url_for('check_task_status', task_id=task.task_id))
+
+
+@app.route('/status/<string:task_id>/')
+def check_task_status(task_id):
+    result = factorial.AsyncResult(task_id)
+    return render_template('status.html', result=result)
+
--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+from time import sleep
 from celery import Celery
 
 celery_app = Celery(__name__, backend='redis://localhost', broker='redis://localhost')
@@ -8,4 +9,5 @@ def factorial(n):
     def f(i):
         return 1 if i < 2 else i * f(i - 1)
 
+    sleep(15)
     return f(n)
